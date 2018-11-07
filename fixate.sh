9c67bf9b04b1e9de50f593b8b3216ee8eb5be05c
Yan Wang <ywang@max.md>
2018-11-07 16:44:22 -0500

    9

--- fixate.sh
+++ fixate.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+env="$1"
+
+echo "env: $env"
 
 mvn clean install
 
+version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[' | grep -v 'Downloading:'`
 
-cp target/hello-world-*  /home/maxmdsys/tmp/.
+
+cp target/hello-world-"$version".jar  /home/maxmdsys/tmp/hello-world-"$version"-"$env".jar
+#cp target/hello-world-"$version".jar  /home/maxmdsys/tmp/.
 
 
