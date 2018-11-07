1a50d5d9f244ee637e764c696439642cc55e2613
Yan Wang <ywang@max.md>
2018-11-07 13:43:08 -0500

    test

--- src/main/java/org/maxmd/test/helloworld/A.java
+++ src/main/java/org/maxmd/test/helloworld/A.java
@@ -5,7 +5,7 @@ import java.util.Date;
 class A {
     public static void main(String[] args) {
         System.out.println(new Date());
-        System.out.println("1.0.2");
+        System.out.println("1.0.3");
 
     }
 
